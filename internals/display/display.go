497287a68734d4b55bc9c31596133bd64ddabdfa
nicholasgswan <nicholasgswan@gmail.com>
2026-07-07 21:22:42 -0700

    add render scaling, group points to draw and call DrawPoints instead of DrawPoint on each

--- internals/display/display.go
+++ internals/display/display.go
@@ -9,9 +9,11 @@ import (
 var window *sdl.Window
 var rend *sdl.Renderer
 
+const scale int32 = 16
+
 func init() {
 	var err error
-	window, err = sdl.CreateWindow("Testing SDL2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 64, 32, sdl.WINDOW_SHOWN)
+	window, err = sdl.CreateWindow("Testing SDL2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 64*scale, 32*scale, sdl.WINDOW_SHOWN)
 	if err != nil {
 		fmt.Printf("could not open window %s\n", err)
 		panic(err)
@@ -26,11 +28,20 @@ func init() {
 }
 
 func DrawDisplay(pixels [32][64]bool) {
+	points := make([]sdl.Point, 0, 0)
 	for y := 0; y < len(pixels); y++ {
 		for x := 0; x < len(pixels[0]); x++ {
 			if pixels[y][x] {
 				rend.SetDrawColor(255, 255, 255, 255)
-				rend.DrawPoint(int32(x), int32(y))
+				xDraw := x * int(scale)
+				yDraw := y * int(scale)
+				for i := 0; i < int(scale); i++ {
+					for j := 0; j < int(scale); j++ {
+
+						points = append(points, sdl.Point{X: int32(xDraw + i), Y: int32(yDraw + j)})
+					}
+				}
+				//rend.DrawPoint(int32(x), int32(y))
 
 			} else {
 				fmt.Print("00")
@@ -38,6 +49,7 @@ func DrawDisplay(pixels [32][64]bool) {
 		}
 		fmt.Print("\n")
 	}
+	rend.DrawPoints(points)
 	rend.Present()
 }
 
